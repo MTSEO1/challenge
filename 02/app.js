e16e6c5bc618460bb49f943913d969b70a65325a
MTSEO1 <seomt992@gmail.com>
2022-08-05 23:40:17 +0900

    day 02

--- 02/app.js
+++ 02/app.js
@@ -4,15 +4,15 @@ bgColor.className = 'color-1';
 function resizePage(event) {
   const pageWidth = event.target.innerWidth;
   console.log(event.target.innerWidth);
-  if (pageWidth <= 650) {
+  if (pageWidth <= 600) {
     bgColor.classList.remove('color-2');
     bgColor.classList.remove('color-3');
     bgColor.classList.add('color-1');
-  } else if (pageWidth <= 950) {
+  } else if (pageWidth <= 900) {
     bgColor.classList.remove('color-1');
     bgColor.classList.remove('color-3');
     bgColor.classList.add('color-2');
-  } else if (pageWidth <= 1250) {
+  } else if (pageWidth <= 1200) {
     bgColor.classList.remove('color-1');
     bgColor.classList.remove('color-2');
     bgColor.classList.add('color-3');
